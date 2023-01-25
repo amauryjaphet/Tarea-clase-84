029620066fad8c1de3a475b589dca79afb882934
amauryjaphet <115047834+amauryjaphet@users.noreply.github.com>
2023-01-24 18:46:51 -0600

    Update main_84.js

--- main_84.js
+++ main_84.js
@@ -3,10 +3,10 @@ pizza_list_array = [];
 function getmenu()
 	{    
         // Da un nombre apropiado de identificación como nombre_pizza.
-        var item = document.getElementById("        ").value; 
+        var item = document.getElementById("nombre_pizza").value; 
        
         //Agrega la función push() para llevar los elementos al array.
-        pizza_list_array.      (item);
+        pizza_list_array.push(item);
         
         var remove_commas = pizza_list_array.join("<br># ");
         document.getElementById("mostrar_menú").innerHTML =  "# " + remove_commas;
